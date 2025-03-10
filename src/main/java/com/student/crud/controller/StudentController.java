fff13fc6fd18de7ffd67a52ae4773df636cc5502
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 21:23:11 +0530

    Controller layer.

---
+++ src/main/java/com/student/crud/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.student.crud.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+    // handler method to handle list students request
+
+}
