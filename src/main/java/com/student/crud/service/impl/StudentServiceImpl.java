24f73172fa5e898b5d6125d69f587f7197116e69
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 21:34:43 +0530

    Implemented Service layer interface and methods.

--- src/main/java/com/student/crud/service/impl/StudentServiceImpl.java
+++ src/main/java/com/student/crud/service/impl/StudentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.student.crud.service.impl;
 
 import com.student.crud.dto.StudentDto;
+import com.student.crud.entity.Student;
+import com.student.crud.mapper.StudentMapper;
 import com.student.crud.repository.StudentRepository;
 import com.student.crud.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -21,6 +24,15 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<StudentDto> getAllStudents() {
-        return List.of();
+
+        List<Student> students = studentRepository.findAll();
+
+        List<StudentDto> studentDtos = students.stream()
+                .map((student) -> StudentMapper.mapToStudentDto(student))
+                .collect(Collectors.toList());
+
+        return studentDtos;
     }
+
+
 }
