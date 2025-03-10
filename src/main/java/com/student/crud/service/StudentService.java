80b5e3c20ad91ab7e0252721c129387f499aa7fb
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 21:24:44 +0530

    Implemented Service layer interface and methods.

---
+++ src/main/java/com/student/crud/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.student.crud.service;
+
+import com.student.crud.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<StudentDto> getAllStudents();
+
+}
