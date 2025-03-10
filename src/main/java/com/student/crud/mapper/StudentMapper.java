4198e7bc823d40e8dca1d78635ef9dc483237a63
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 21:19:24 +0530

    StudentMapper class to convert jpa entity to dto and vice-versa.

--- src/main/java/com/student/crud/mapper/StudentMapper.java
+++ src/main/java/com/student/crud/mapper/StudentMapper.java
@@ -1,6 +1,32 @@
 package com.student.crud.mapper;
 
+import com.student.crud.dto.StudentDto;
+import com.student.crud.entity.Student;
+
 public class StudentMapper {
 
+    // convert jpa entity to dto and vice-versa
+
+    public static StudentDto mapToStudentDto(Student student) {
+        StudentDto studentDto = new StudentDto(
+                student.getId(),
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail()
+        );
+
+        return studentDto;
+    }
+
+    public static Student mapToStudent(StudentDto studentDto) {
+        Student student = new Student(
+               studentDto.getId(),
+                studentDto.getFirstName(),
+                studentDto.getLastName(),
+                studentDto.getEmail()
+        );
+
+        return student;
+    }
 
 }
