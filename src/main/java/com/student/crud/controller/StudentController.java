3d02cc8f5a394e6e5c482b5edf12038513e74b4b
Rishib#321 <rishitab2209@gmail.com>
2025-03-10 21:34:59 +0530

    Implemented Service layer interface and methods, controller layer.

--- src/main/java/com/student/crud/controller/StudentController.java
+++ src/main/java/com/student/crud/controller/StudentController.java
@@ -1,10 +1,34 @@
 package com.student.crud.controller;
 
+import com.student.crud.dto.StudentDto;
+import com.student.crud.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
 
+    private StudentService studentService;
+
+    // inject interface as a dependency to maintain the loose coupling
+    // constructor Injection
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     // handler method to handle list students request
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+
+        return "students";
+    }
 
 }
